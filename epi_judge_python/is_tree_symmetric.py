ff5afd9f0586440ba74328f280c06a2336dffe74
Daniel Gerlach <daniel.gerlach1981@googlemail.com>
2021-10-31 21:56:55 +0100

    is binary tree symmetric, problem 9.2

--- epi_judge_python/is_tree_symmetric.py
+++ epi_judge_python/is_tree_symmetric.py
@@ -3,11 +3,21 @@ from test_framework import generic_test
 
 
 def is_symmetric(tree: BinaryTreeNode) -> bool:
-    # TODO - you fill in here.
-    return True
+    def helper(node, mirror):
+        if mirror is None and node is None:
+            return True
+        elif node is None or mirror is None:
+            return False
+        leftSubtree = helper(node.left, mirror.right)
+        rightSubtree = helper(node.right, mirror.left)
+        return leftSubtree and rightSubtree and (node.data == mirror.data)
+
+    return helper(tree, tree)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     exit(
-        generic_test.generic_test_main('is_tree_symmetric.py',
-                                       'is_tree_symmetric.tsv', is_symmetric))
+        generic_test.generic_test_main(
+            "is_tree_symmetric.py", "is_tree_symmetric.tsv", is_symmetric
+        )
+    )
