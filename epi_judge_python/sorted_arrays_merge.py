44ee6a94da01b8ce1fe0ef89890c391b414c3160
Daniel Gerlach <daniel.gerlach1981@googlemail.com>
2021-11-13 15:48:25 +0100

    added merge sorted arrays with files

--- epi_judge_python/sorted_arrays_merge.py
+++ epi_judge_python/sorted_arrays_merge.py
@@ -4,6 +4,8 @@ from test_framework import generic_test
 from collections import namedtuple
 from heapq import heapify, heappop, heappush
 
+import linecache
+
 
 def merge_sorted_arrays(sorted_arrays: List[List[int]]) -> List[int]:
     result: List[int] = []
@@ -16,6 +18,17 @@ def merge_sorted_arrays(sorted_arrays: List[List[int]]) -> List[int]:
             item = sorted_arrays[index_arr][index + 1]
             heappush(heap, (item, index_arr, index + 1))
     return result
+
+
+def merge_sorted_files(sorted_files: List[List[str]]) -> List[int]:
+    sorted_arrays = []
+    for filename in sorted_files:
+        with open(filename) as file:
+            lines = file.readlines()
+            lines = [int(line.strip()) for line in lines if line]
+            sorted_arrays.append(lines)
+
+    return merge_sorted_arrays(sorted_arrays)
 
 
 Case = namedtuple("Case", ["arrs", "expected"])
@@ -32,6 +45,15 @@ def test():
 
     for c in cases:
         actual = merge_sorted_arrays(c.arrs)
+        assert actual == c.expected
+
+
+def test_files():
+    files = ["foo_1.txt", "foo_2.txt", "foo_3.txt"]
+    cases = [Case(files, [1, 1, 1, 2, 2, 3, 4, 4, 5, 6])]
+
+    for c in cases:
+        actual = merge_sorted_files(c.arrs)
         assert actual == c.expected
 
 
