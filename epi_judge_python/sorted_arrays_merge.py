a8e9d077193c2e970cf22a4cf773137f9724155a
Daniel Gerlach <daniel.gerlach1981@googlemail.com>
2021-11-13 15:23:59 +0100

    problem 10.1, merge k sorted arrays

--- epi_judge_python/sorted_arrays_merge.py
+++ epi_judge_python/sorted_arrays_merge.py
@@ -1,15 +1,43 @@
 from typing import List
 
 from test_framework import generic_test
+from collections import namedtuple
+from heapq import heapify, heappop, heappush
 
 
 def merge_sorted_arrays(sorted_arrays: List[List[int]]) -> List[int]:
-    # TODO - you fill in here.
-    return []
+    result: List[int] = []
+    heap = [(arr[0], i, 0) for i, arr in enumerate(sorted_arrays) if arr]
+    heapify(heap)
+    while heap:
+        val, index_arr, index = heappop(heap)
+        result.append(val)
+        if index < len(sorted_arrays[index_arr]) - 1:
+            item = sorted_arrays[index_arr][index + 1]
+            heappush(heap, (item, index_arr, index + 1))
+    return result
 
 
-if __name__ == '__main__':
+Case = namedtuple("Case", ["arrs", "expected"])
+
+
+def test():
+    cases = [
+        Case(
+            [[1, 2], [3, 4], [1, 3]],
+            [1, 1, 2, 3, 3, 4],
+        ),
+        Case([[1, 2, 3], [], [1]], [1, 1, 2, 3]),
+    ]
+
+    for c in cases:
+        actual = merge_sorted_arrays(c.arrs)
+        assert actual == c.expected
+
+
+if __name__ == "__main__":
     exit(
-        generic_test.generic_test_main('sorted_arrays_merge.py',
-                                       'sorted_arrays_merge.tsv',
-                                       merge_sorted_arrays))
+        generic_test.generic_test_main(
+            "sorted_arrays_merge.py", "sorted_arrays_merge.tsv", merge_sorted_arrays
+        )
+    )
