26c20f69a6c7c225437b35662dfd698fce111995
Daniel Gerlach <daniel.gerlach1981@googlemail.com>
2021-12-14 15:37:19 +0100

    check if tree is BST

--- epi_judge_python/is_tree_a_bst.py
+++ epi_judge_python/is_tree_a_bst.py
@@ -2,12 +2,59 @@ from binary_tree_node import BinaryTreeNode
 from test_framework import generic_test
 
 
+def str_bst(node):
+    """String representation of a binary tree"""
+    if not node:
+        return "None"
+    if not node.data:
+        return "No data"
+    return "{} ({} {})".format(node.data, rec_str(node.left), rec_str(node.right))
+
+
 def is_binary_tree_bst(tree: BinaryTreeNode) -> bool:
-    # TODO - you fill in here.
-    return True
+    if not tree:
+        return True
+
+    def helper(node):
+        if node.left:
+            left, left_max, left_min = helper(node.left)
+            if not left:
+                return False, 0, 1
+
+        if node.right:
+            right, right_max, right_min = helper(node.right)
+            if not right:
+                return False, 0, 1
+
+        if node.left and node.right:
+            if left_max <= node.data and right_min >= node.data:
+                return (
+                    True,
+                    max([node.data, right_max]),
+                    min([node.data, left_min]),
+                )
+            else:
+                return False, 2, 3
+        elif node.left:
+            if left_max <= node.data:
+                return True, node.data, min(node.data, left_min)
+            else:
+                return False, 4, 5
+        elif node.right:
+            if right_min >= node.data:
+                return True, max(node.data, right_max), node.data
+            else:
+                return False, 6, 7
+        else:
+            return True, node.data, node.data
+
+    is_bst, _, _ = helper(tree)
+    return is_bst
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     exit(
-        generic_test.generic_test_main('is_tree_a_bst.py', 'is_tree_a_bst.tsv',
-                                       is_binary_tree_bst))
+        generic_test.generic_test_main(
+            "is_tree_a_bst.py", "is_tree_a_bst.tsv", is_binary_tree_bst
+        )
+    )
