aa501e31b0b14847308e9e8c6db5329bd08554b0
Daniel Gerlach <daniel.gerlach1981@googlemail.com>
2021-12-14 14:22:11 +0100

    merged two sorted arrays in place

--- epi_judge_python/intersect_sorted_arrays.py
+++ epi_judge_python/intersect_sorted_arrays.py
@@ -1,15 +1,59 @@
 from typing import List
+from collections import namedtuple
 
 from test_framework import generic_test
 
 
 def intersect_two_sorted_arrays(A: List[int], B: List[int]) -> List[int]:
-    # TODO - you fill in here.
-    return []
+
+    if not A or not B:
+        return []
+
+    result: List[int] = []
+    i = j = 0
+    while i < len(A) and j < len(B):
+        if A[i] < B[j]:
+            i += 1
+        elif B[j] < A[i]:
+            j += 1
+        else:
+            # equal case
+            val = A[i]
+            i += 1
+            j += 1
+            if result and val == result[-1]:
+                continue
+            else:
+                result.append(val)
+    return result
 
 
-if __name__ == '__main__':
+Case = namedtuple("Case", ["A", "B", "expected"])
+
+
+def test_simple():
+    cases = [
+        Case([1, 2, 3], [2, 7, 8], [2]),
+        Case([], [1, 2, 3], []),
+        Case([2, 3, 3, 4], [2, 2, 2, 5], [2]),
+        Case(
+            [2, 3, 3, 5, 5, 6, 7, 7, 8, 12],
+            [5, 5, 6, 8, 8, 9, 10, 10],
+            [5, 6, 8],
+        ),
+    ]
+
+    for c in cases:
+        actual = intersect_two_sorted_arrays(c.A, c.B)
+
+        assert actual == c.expected, "Case: {} {}".format(c.A, c.B)
+
+
+if __name__ == "__main__":
     exit(
-        generic_test.generic_test_main('intersect_sorted_arrays.py',
-                                       'intersect_sorted_arrays.tsv',
-                                       intersect_two_sorted_arrays))
+        generic_test.generic_test_main(
+            "intersect_sorted_arrays.py",
+            "intersect_sorted_arrays.tsv",
+            intersect_two_sorted_arrays,
+        )
+    )
--- epi_judge_python/two_sorted_arrays_merge.py
+++ epi_judge_python/two_sorted_arrays_merge.py
@@ -1,12 +1,60 @@
-from typing import List
+from typing import List, Deque
 
 from test_framework import generic_test
 
+from collections import namedtuple, deque
 
-def merge_two_sorted_arrays(A: List[int], m: int, B: List[int],
-                            n: int) -> None:
-    # TODO - you fill in here.
-    return
+
+def solution(A: List[int], m: int, B: List[int], n: int) -> None:
+    a, b, write_idx = m - 1, n - 1, m + n - 1
+
+    while a >= 0 and b >= 0:
+        if A[a] > B[b]:
+            A[write_idx] = A[a]
+            a -= 1
+            write_idx -= 1
+        else:
+            A[write_idx] = B[b]
+            b -= 1
+            write_idx -= 1
+
+    while b >= 0:
+        A[write_idx] = B[b]
+        b -= 1
+        write_idx -= 1
+
+    return None
+
+
+def solution2(A: List[int], m: int, B: List[int], n: int) -> None:
+    tmp = []
+    i = j = 0
+
+    while i < m and j < n:
+        if A[i] <= B[j]:
+            tmp.append(A[i])
+            i += 1
+        elif B[j] < A[i]:
+            tmp.append(B[j])
+            j += 1
+
+    while j < n:
+        tmp.append(B[j])
+        i += 1
+        j += 1
+
+    while i < m:
+        tmp.append(A[i])
+        i += 1
+        j += 1
+
+    for k in range(n + m):
+        A[k] = tmp[k]
+    return None
+
+
+def merge_two_sorted_arrays(A: List[int], m: int, B: List[int], n: int) -> None:
+    solution(A, m, B, n)
 
 
 def merge_two_sorted_arrays_wrapper(A, m, B, n):
@@ -14,8 +62,30 @@ def merge_two_sorted_arrays_wrapper(A, m, B, n):
     return A
 
 
-if __name__ == '__main__':
+Case = namedtuple("Case", ["A", "m", "B", "n", "expected"])
+
+
+def test():
+    cases = [
+        Case(
+            [3, 13, 17, 0, 0, 0, 0, 0],
+            3,
+            [3, 7, 11, 19],
+            4,
+            [3, 3, 7, 11, 13, 17, 19, 0],
+        )
+    ]
+
+    for c in cases:
+        merge_two_sorted_arrays(c.A, c.m, c.B, c.n)
+        assert c.A == c.expected, "Case: {} {}".format(c.A, c.B)
+
+
+if __name__ == "__main__":
     exit(
-        generic_test.generic_test_main('two_sorted_arrays_merge.py',
-                                       'two_sorted_arrays_merge.tsv',
-                                       merge_two_sorted_arrays_wrapper))
+        generic_test.generic_test_main(
+            "two_sorted_arrays_merge.py",
+            "two_sorted_arrays_merge.tsv",
+            merge_two_sorted_arrays_wrapper,
+        )
+    )
